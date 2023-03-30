12951b0798b6df01c52fbb4430b688b46540dfcb
Pedro Henrique Penna <ppenna@microsoft.com>
2023-03-30 12:09:52 -0300

    [test] Feature: Initial Test Program

---
+++ test/Program.cs
@@ -0,0 +1,10 @@
+﻿using Demikernel;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Demikernel.Server.Initialize(args);
+        System.Console.WriteLine("Hello, World!");
+    }
+}
